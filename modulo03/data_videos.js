a010843d4fba38a3a82af05bedd4d32fa9a8ed66
Felipe Costa <felipeiasi@gmail.com>
2020-02-08 07:56:47 -0300

    Ajuste data.js

--- modulo03/data_videos.js
+++ modulo03/data_videos.js
@@ -3,37 +3,39 @@ module.exports = [
         id: "7Rw1PdcLUTg",
         title: "Vizrt introdução - Aula 1",
         duration: "12 min",
-        image:"https://img.youtube.com/vi/7Rw1PdcLUTg/maxresdefault.jpg",
+        image: "https://img.youtube.com/vi/7Rw1PdcLUTg/maxresdefault.jpg",
         price: "Free"
-    },{
+    }, {
         id: "7Rw1PdcLUTg",
         title: "Vizrt introdução - Aula 1",
         duration: "10 min",
-        image:"https://img.youtube.com/vi/7Rw1PdcLUTg/maxresdefault.jpg",
-        price: "Free"
-    },{
+        image: "https://img.youtube.com/vi/7Rw1PdcLUTg/maxresdefault.jpg",
+        price: "Free",
+        featured: true
+    }, {
         id: "7Rw1PdcLUTg",
         title: "Vizrt introdução - Aula 1",
         duration: "10 min",
-        image:"https://img.youtube.com/vi/7Rw1PdcLUTg/maxresdefault.jpg",
+        image: "https://img.youtube.com/vi/7Rw1PdcLUTg/maxresdefault.jpg",
         price: "Free"
-    },{
+    }, {
         id: "7Rw1PdcLUTg",
         title: "Vizrt introdução - Aula 1",
         duration: "10 min",
-        image:"https://img.youtube.com/vi/7Rw1PdcLUTg/maxresdefault.jpg",
+        image: "https://img.youtube.com/vi/7Rw1PdcLUTg/maxresdefault.jpg",
         price: "Free"
-    },{
+    }, {
         id: "7Rw1PdcLUTg",
         title: "Vizrt introdução - Aula 1",
         duration: "10 min",
-        image:"https://img.youtube.com/vi/7Rw1PdcLUTg/maxresdefault.jpg",
-        price: "Free"
-    },{
+        image: "https://img.youtube.com/vi/7Rw1PdcLUTg/maxresdefault.jpg",
+        price: "Free",
+        featured: true
+    }, {
         id: "7Rw1PdcLUTg",
         title: "Vizrt introdução - Aula 1",
         duration: "10 min",
-        image:"https://img.youtube.com/vi/7Rw1PdcLUTg/maxresdefault.jpg",
+        image: "https://img.youtube.com/vi/7Rw1PdcLUTg/maxresdefault.jpg",
         price: "Free"
-    }, 
+    },
 ]
