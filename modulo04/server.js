4769435c7b2544f3887366156a0dd3da68d83d7d
Felipe Costa <felipeiasi@gmail.com>
2020-02-12 23:04:01 -0300

    acertando rotas

--- modulo04/server.js
+++ modulo04/server.js
@@ -1,17 +1,19 @@
 const express = require('express')
-const nunjuncks = require('nunjucks')
+const nunjucks = require('nunjucks')
 const routes = require('./routes')
 
 const server = express()
 
 server.use(express.static('public'))
+
 server.use(routes)
 
 server.set("view engine", "njk")
 
-nunjuncks.configure("views",{
+nunjucks.configure("views", {
     express: server,
-    autoescape: false
+    autoescape: false,
+    noCache: true
 })
 
 server.listen(3000, function(){
