18a4bc6d598e592fd303c77faa8c84bd89a01a7a
Felipe Costa <felipeiasi@gmail.com>
2021-01-03 22:30:11 +0100

    modulo 04

---
+++ Modulo04/routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const routes = express.Router();
+
+routes.get("/",function(req,res){
+    return res.redirect("/instructors");
+})
+
+routes.get("/instructors",function(req,res){
+    return res.render("instructors/index");
+})
+
+routes.get("/instructors/create",function(req,res){
+    return res.render("instructors/create");
+})
+
+routes.post("/instructors/create",function(req,res){
+    //req.query
+    //req.body
+
+    return res.send(req.body);
+})
+
+routes.get("/members",function(req,res){
+    return res.send("/members");
+})
+
+module.exports = routes;
