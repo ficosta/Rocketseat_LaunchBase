ca4a872e83b6c5ab4625f1752a2919b54a9b786b
Felipe Costa <felipeiasi@gmail.com>
2021-01-03 14:29:38 +0100

    modulo 03

---
+++ Modulo03/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "EhnXaybirdA",
+        title: "Formulario animado com JS",
+        duration: "57min",
+        price: "teste"
+    },
+    {
+        id: "EhnXaybirdA",
+        title: "Formulario animado com JS",
+        duration: "57min",
+        price: "teste"
+    }, 
+    {
+        id: "EhnXaybirdA",
+        title: "Formulario animado com JS",
+        duration: "57min",
+        price: "teste"
+    }, 
+    {
+        id: "EhnXaybirdA",
+        title: "Formulario animado com JS",
+        duration: "57min",
+        price: "Free"
+    }, 
+    {
+        id: "EhnXaybirdA",
+        title: "Formulario animado com JS",
+        duration: "57min",
+        price: "Pago"
+    }, 
+    {
+        id: "DAsdAD",
+        title: "Formulario animado com JS",
+        duration: "57min",
+        price: "teste"
+    },
+]
---
+++ Modulo03/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const nunjucks = require("nunjucks");
+
+const server = express();
+const videos = require("./data")
+
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express:server
+})
+
+server.get("/",function(req,res){
+    return res.render("about");
+})
+
+server.get("/portfolio",function(req,res){
+    return res.render("classes", {items: videos});
+})
+
+server.listen(5000, function(){
+    console.log("Servidor está rodando");
+})
